6df1efc2af9e1200a8cfe2182716589c9e3f00a8
Shermaine <shermaineang2802@gmail.com>
2023-03-22 03:39:32 +0800

    Leaderboard stuff

--- game/Assets/Examples/readme.md
+++ game/Assets/Examples/readme.md
@@ -1,34 +1,22 @@
-#AWS SDK for Unity DynamoDB Sample
-
-The AWS SDK for Unity DynamoDB  uses the new UI Event System, so it requires Unity version >4.6 to run the sample. 
-
-#####Configure the DynamoDB Sample
-Open the HighLevelExample, LowLevelDynamoDBExample, and the TableQueryAndScanExample scenes and perform the following for each:
-
-
-* Click the “HighLevel”, “LowLevel”, or “QueryAndScan” game object in "Hierarchy Pane".
-* Add "Identity Pool Id" in "Inspector Pane". A pool can be created in the [Cognito console](https://console.aws.amazon.com/cognito/home).
-* If your Cognito Identity Pool is in a region other than "us-east-1", change the "Cognito Identity Region" value in "Inspector Pane".
-* If you want to use a region other than "us-east-1" for DynamoDB, you can change the "Dynamo Region" value as well.
-* Set up a "link.xml" file as described on the [Unity SDK readme](https://github.com/aws/aws-sdk-net/blob/master/Unity.README.md#unity-sdk-fundamentals), which is necessary if you will be building with assembly stripping or IL2CPP. Be sure to add the line `<assembly fullname="AWSSDK.DynamoDBv2" preserve="all"/>`.
+# Amazon Cognito Sync sample
 
 
 
-Then in the Unity Menu go to File->Build Settings and in ‘Scenes to Build’, add the scenes in the following order
+This sample shows how to store data in the cloud using Amazon Cognito Sync. The sample demonstrates storing two text fields: player name and player alias.
 
-
-    1. DynamoDbExample.unity
-    2. LowLevelDynamoDbExample.unity
-    3. TableQueryAndScanExample.unity
-    4. HighLevelExample.unity   
-
-This will allow you to execute all the operation as a single game.
-
-* The project will create 4 tables - ProductCatalog, Forum, Thread & Reply. Make sure that these tables are not already present. You can check the tables at the [DynamoDB console]( https://console.aws.amazon.com/dynamodb/).
-
+#####Configure the Cognito Sync Sample
+1. Open the CognitoSyncManagerSample scene file.
+2. Click "CognitoSyncManagerSample" game object in "Hierarchy Pane".
+3. Add "Identity Pool Id" in "Inspector Pane". A pool can be created in the [Cognito console]( https://console.aws.amazon.com/cognito/home).
+4. If your Cognito Identity Pool is in a region other than "us-east-1", change the "Region" value in "Inspector Pane".
+5. Set up a "link.xml" file as described on the [Unity SDK readme](https://github.com/aws/aws-sdk-net/blob/master/Unity.README.md#unity-sdk-fundamentals), which is necessary if you will be building with assembly stripping or IL2CPP. Be sure to add the line `<assembly fullname="AWSSDK.CognitoSync" preserve="all"/>`.
 
 
 #####Run the sample scene
-1. Open the DynamoDbExample.unity scene.
-2. Click the “Run” button
+1. Click the "Run" button. 
 
+2. When the "Save offline" button is pressed, the two fields are stored in a Cognito Sync dataset, but not pushed to the cloud yet. This is the way you persist data locally.
+
+3. The "Sync with Amazon Cognito" button will synchronize (push and/or pull) the changes between your local dataset and the cloud.
+
+4. "Delete local data" will wipe the local dataset, but you will still be able to recover the data from the cloud by synchronizing.
--- game/Assets/Scripts/Leaderboard.cs
+++ game/Assets/Scripts/Leaderboard.cs
@@ -1,11 +1,149 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.DocumentModel;
+using System.Collections.Generic;
+using Amazon.CognitoIdentity;
 using TMPro;
+
+
 
 public class Leaderboard : MonoBehaviour{
 
-    [SerializeField]
+    public TMP_InputField userNameInput;
+    private AmazonDynamoDBClient client;
+    private DynamoDBContext Context;
+
+    void Start()
+    {
+        // Initialize the Amazon DynamoDB client
+        Debug.Log("AAAAAAAAAAA");
+        UnityInitializer.AttachToGameObject(this.gameObject);
+        Debug.Log("LOLZIES");
+        try
+        {
+            CognitoAWSCredentials credentials = new CognitoAWSCredentials("us-east-1:9f9f53aa-e0c2-48aa-a3e7-e3ee0a519d74", RegionEndpoint.USEast1);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Failed to initialize DynamoDB client: " + ex.Message);
+        }
+        
+        // Set up the Amazon DynamoDB client
+        Debug.Log("BBBBBBBBBB");
+        //client = new AmazonDynamoDBClient(credentials);
+        Debug.Log("CCCCCCCCC");
+        //Context = new DynamoDBContext(client);
+        Debug.Log("INITIALISED???");
+    }
+
+    public void SaveScore(string username, int score)
+    {
+        // Create a new leaderboard item
+        var item = new LeaderboardItem
+        {
+            Username = userNameInput.text,
+            Score = score
+        };
+
+        // Save the item to the database
+        Context.SaveAsync(item, (result) =>
+        {
+            if (result.Exception != null)
+            {
+                Debug.LogError(result.Exception);
+            }
+            else
+            {
+                Debug.Log("Score saved successfully");
+            }
+        });
+
+    }
+
+
+    
+    public void getLeaderboard(int limit, System.Action<List<LeaderboardItem>> callback)
+    {
+        // Set up the query request
+        var request = new QueryRequest
+        {
+            TableName = "TestingTable",
+            KeyConditionExpression = "LeaderboardId = :lbid",
+            ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+            {
+                {":lbid", new AttributeValue { S = "1" }}
+            },
+            Limit = limit,
+            ScanIndexForward = false // Sort the results in descending order
+        };
+
+        // Query the database
+        client.QueryAsync(request, (result) =>
+        {
+            if (result.Exception != null)
+            {
+                Debug.LogError(result.Exception);
+                callback(null);
+            }
+            else
+            {
+                // Convert the results to leaderboard items
+                List<LeaderboardItem> items = new List<LeaderboardItem>();
+                foreach (var item in result.Response.Items)
+                {
+                    var leaderboardItem = new LeaderboardItem();
+                    leaderboardItem.FromDocument(item);
+                    items.Add(leaderboardItem);
+                }
+
+
+                
+
+
+                // Invoke the callback with the leaderboard items
+                callback(items);
+            }
+        });
+    }
+}
+
+
+    [DynamoDBTable("TestingTable")]
+    public class LeaderboardItem
+    {
+        [DynamoDBHashKey]
+        public string Username { get; set; }
+
+        [DynamoDBProperty]
+        public int Score { get; set; }
+
+        [DynamoDBIgnore]
+        public int Rank { get; set; }
+
+        public void FromDocument(Dictionary<string, AttributeValue> documentAttributes)
+        {
+            var document = Document.FromAttributeMap(documentAttributes);
+            Username = document["Username"];
+            Score = int.Parse(document["Score"]);
+            Rank = int.Parse(document["Rank"]);
+        }
+
+
+        public Document ToDocument()
+        {
+            var document = new Document();
+            document["Username"] = Username;
+            document["Score"] = Score.ToString();
+            document["Rank"] = Rank.ToString();
+            return document;
+        }
+    }
+
+
+    /* [SerializeField]
     private List<TextMeshProUGUI> names;
 
     [SerializeField]
@@ -24,10 +162,12 @@ public class Leaderboard : MonoBehaviour{
 
 
         */
-    }
+    //}
 
-    public void setleaderboardentry(string username, int score){
+    //public void setleaderboardentry(string username, int score){
         //upload to server new username and score
-    }
+    //}
 
-}
+
+
+
